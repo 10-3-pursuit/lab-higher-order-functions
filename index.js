ef3b567efb27c6d0d4b5d1ecfea49d21655d8f73
Cristian Garcia <crisg@sas.upenn.edu>
2023-10-01 19:58:32 -0400

    second function

--- index.js
+++ index.js
@@ -51,7 +51,11 @@ function filter(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    if (callback(element) === true) {
+      result.push(element);
+    }
   }
+  return result;
 }
 
 /**
